bd8643973ad22c8a50293cfffdd62b7e5f31f2ce
huang <day_1111@126.com>
2017-03-11 14:48:39 +0800

    login

--- src/com/joe/myblog/oa/controller/BaseController.java
+++ src/com/joe/myblog/oa/controller/BaseController.java
@@ -4,7 +4,31 @@
 */
 package com.joe.myblog.oa.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+
 public class BaseController {
 
+	protected HttpServletRequest request;
+
+	protected HttpServletResponse response;
+
+	protected HttpSession session;
+	
+	@ModelAttribute
+	public void initController(HttpServletRequest request, HttpServletResponse response) {
+		this.request = request;
+		this.response = response;
+		this.session = request.getSession();
+	}
+	
+	protected Integer getLogUserId() {
+		
+		return 1;
+	}
 }
 
--- src/com/joe/myblog/oa/controller/UserController.java
+++ src/com/joe/myblog/oa/controller/UserController.java
@@ -4,7 +4,43 @@
 */
 package com.joe.myblog.oa.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sun.swing.internal.plaf.metal.resources.metal;
+
+
+@Controller
 public class UserController {
 
+	/**
+	* Title: userLoginPage
+	* Description: 去登录页面
+	* @return
+	* @author HuangJian
+	* @date 2017年3月9日
+	*/
+	@RequestMapping(value="/blog")
+	public String userLoginPage(){
+		
+		
+		return "/login/login";
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/blog/bloglogin")
+	public String blogLogin(){
+		
+		
+		return "redirect:/blog/home";
+	}
+	
+	@RequestMapping(value = "/blog/home")
+	public String loginHome(Model model){
+		
+		
+		return "/layout/bloghome";
+	}
 }
 
